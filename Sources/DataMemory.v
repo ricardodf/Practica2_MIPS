30ae8a513ecb35fbc960aec3a7467c8ae12e9c89
Omartian <omartian21@gmail.com>
2020-03-30 21:20:33 -0600

    Se le resta 1001_0000 a la dirección de la ram

--- Sources/DataMemory.v
+++ Sources/DataMemory.v
@@ -25,6 +25,8 @@ module DataMemory
 	// Declare the RAM variable
 	reg [DATA_WIDTH-1:0] ram[MEMORY_DEPTH-1:0];
 	wire [DATA_WIDTH-1:0] ReadDataAux;
+	wire [(DATA_WIDTH-1):0] newAddress;
+	assign newAddress = Address - 32'h1001_0000;
 
 	always @ (posedge clk)
 	begin
